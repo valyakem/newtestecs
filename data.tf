d9df4f758a1e080abf497e612e3c69ccd26b367a
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-11 08:47:09 -0500

    Testing continues

--- data.tf
+++ data.tf
@@ -1,19 +1,5 @@
 data "aws_iam_role" "pipeline_role" {
   name = "codepipeline-role"
-    assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "codepipeline.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
 }
 
 data "aws_iam_role" "ecs-task" {
