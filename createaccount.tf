6432f6f493aa42c65e1f255473e222f730d82833
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-16 13:51:58 -0400

    trying after changing region

--- createaccount.tf
+++ createaccount.tf
@@ -5,7 +5,7 @@ resource "aws_organizations_account" "pricingtool-arcablanca" {
   iam_user_access_to_billing  = "ALLOW"
   parent_id = "r-tjwo"
 
-  # There is no AWS Organizations API for reading role_name
+  # There is no AWS Organizations API for reading role_name change region
   lifecycle {
     ignore_changes = [role_name]
   }
