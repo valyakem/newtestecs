b19f000e37c9c135df1ffd8407580d489617e307
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-16 13:39:36 -0400

    Trying some data addition here

--- createaccount.tf
+++ createaccount.tf
@@ -2,6 +2,8 @@ resource "aws_organizations_account" "pricingtool-arcablanca" {
   name      = "pricingtool-arcablanca"
   email     = "valentine.akem@nexgbits.com"
   role_name = "AWSControlTowerAdmin"
+  iam_user_access_to_billing  = "ALLOW"
+  parent_id = "r-tjwo"
 
   # There is no AWS Organizations API for reading role_name
   lifecycle {
