b0f09ebc09d6b3e2634416a273aaefa6b2862beb
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-16 12:16:24 -0400

    Added some information

--- CICD-pipeline.tf
+++ CICD-pipeline.tf
@@ -86,7 +86,7 @@ resource "aws_codepipeline" "pipeline" {
 
       configuration = {
         ClusterName = "clusterDev"
-        ServiceName = "nbapp-Service"
+        ServiceName = "golang-Service"
         FileName    = "imagedefinitions.json"
       }
     }
